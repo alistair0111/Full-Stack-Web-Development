2faeadf5409aeb7274024344d41eb4d353615328
alistair0111 <alistair.baretto@gmail,com>
2020-09-19 06:02:53 +0530

    Redux Thunk

--- Front-End Web Development with React/Week 4/confusion/src/components/DishDetailComponent.js
+++ Front-End Web Development with React/Week 4/confusion/src/components/DishDetailComponent.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 // import CommentForm from './CommentFormComponent';
 import {Modal, ModalHeader, Label, Button, ModalBody, Row, Col } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-
+import { Loading } from './LoadingComponent';
 
 
 
@@ -48,7 +48,24 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
     }
   
     const DishDetail = (props) => {
-      console.log('DishDetail Component Render Invoked')
+      if (props.isLoading) {
+        return(
+          <div className="container">
+            <div className="row">
+              <Loading/>
+            </div> 
+          </div>
+          );
+      }
+      else if (props.errMess) {
+        return(
+          <div className="container">
+            <div className="row">
+              <h4>{ props.errMess }</h4>
+            </div> 
+          </div>
+          );
+      }
       if (props.dish != null) {
         return (
         <div className='container'>  
