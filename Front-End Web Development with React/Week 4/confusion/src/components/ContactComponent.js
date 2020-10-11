810ebaf908a2d45125098323a2617fd95b3d538e
alistair0111 <alistair.baretto@gmail,com>
2020-10-11 15:39:43 +0530

    Completion Of Course 2

--- Front-End Web Development with React/Week 4/confusion/src/components/ContactComponent.js
+++ Front-End Web Development with React/Week 4/confusion/src/components/ContactComponent.js
@@ -28,6 +28,7 @@ class Contact extends Component {
         // alert("Current State is "+ JSON.stringify(values));
         this.props.postFeedback(values.firstname, values.lastname, values.telnum, values.email, values.agree, values.contactType, values.message);
         this.props.resetFeedbackForm();
+        
     }
 
 
