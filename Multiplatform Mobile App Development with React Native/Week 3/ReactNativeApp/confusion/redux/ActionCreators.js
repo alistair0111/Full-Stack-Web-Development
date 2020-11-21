c2a25d32e4a7a529bbe57498998706a9694eba40
alistair0111 <alistair.baretto@gmail,com>
2020-11-21 21:25:57 +0530

    Delete Favorites

--- Multiplatform Mobile App Development with React Native/Week 3/ReactNativeApp/confusion/redux/ActionCreators.js
+++ Multiplatform Mobile App Development with React Native/Week 3/ReactNativeApp/confusion/redux/ActionCreators.js
@@ -199,3 +199,8 @@ export const postComment = (dishId, rating, author, comment) => (dispatch) => {
     type: ActionTypes.ADD_COMMENT,
     payload: comment
   });
+
+  export const deleteFavorite = (dishId) => ({
+      type: ActionTypes.DELETE_FAVORITE,
+      payload: dishId
+  });
