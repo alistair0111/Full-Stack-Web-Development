8eb8d55bf64c53ac4b01c592c4b85a2775255bdc
alistair0111 <alistair.baretto@gmail,com>
2020-11-22 21:04:20 +0530

    Animations

--- Multiplatform Mobile App Development with React Native/Week 3/ReactNativeApp/confusion/components/HomeComponent.js
+++ Multiplatform Mobile App Development with React Native/Week 3/ReactNativeApp/confusion/components/HomeComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, Animated, Easing, ScrollView} from 'react-native';
 import { Card, Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -53,29 +53,77 @@ function RenderItem(props) {
 
 class Home extends Component {
     
+    constructor(props) {
+        super(props);
+        this.animatedValue = new Animated.Value(0);
+    }
+
     static navigationOptions = {
         title: 'Home'
     };
 
+    componentDidMount() {
+        this.animate();
+    }
+
+    animate() {
+        this.animatedValue.setValue(0);
+        Animated.timing(
+            this.animatedValue,
+            {
+                toValue: 8,
+                duration: 8000,
+                easing: Easing.linear
+            }
+        ).start()
+    }
+
     render() {
+        const xpos1 = this.animatedValue.interpolate(
+            {
+                inputRange: [0,1,3,5,8],
+                outputRange: [1200,600,0,-600,0]
+            }
+        )
+
+        const xpos2 = this.animatedValue.interpolate(
+            {
+                inputRange: [0,2,4,6,8],
+                outputRange: [1200,600,0,-600,0]
+            }
+        )
+
+        const xpos3 = this.animatedValue.interpolate(
+            {
+                inputRange: [0,3,5,7,8],
+                outputRange: [1200,600,0,-600,0]
+            }
+        )
+
         return (
-            <ScrollView>
-                <RenderItem
-                    item = { this.props.dishes.dishes.filter((dish) => dish.featured)[0] }
-                    errMess={this.props.dishes.errMess}
-                    isLoading={this.props.dishes.isLoading}
-                    />
-                <RenderItem
-                    item = { this.props.promotions.promotions.filter((promo) => promo.featured)[0] }
-                    errMess={this.props.promotions.errMess}
-                    isLoading={this.props.promotions.isLoading}
-                    />
-                <RenderItem
-                    item = { this.props.leaders.leaders.filter((leader) => leader.featured)[0] }
-                    errMess={this.props.leaders.errMess}
-                    isLoading={this.props.leaders.isLoading}
-                    />
-            </ScrollView> 
+            <View style={{ flex: 1,flexDirection: 'column', justifyContent: 'flex-start'}}>
+                <Animated.View style={{width: '100%', transform: [{ translateX: xpos1}]}}>
+                    <RenderItem
+                        item = { this.props.dishes.dishes.filter((dish) => dish.featured)[0] }
+                        errMess={this.props.dishes.errMess}
+                        isLoading={this.props.dishes.isLoading}
+                        />
+                </Animated.View>
+                <Animated.View style={{width: '100%', transform: [{ translateX: xpos2}]}}>
+                    <RenderItem
+                        item = { this.props.promotions.promotions.filter((promo) => promo.featured)[0] }
+                        errMess={this.props.promotions.errMess}
+                        isLoading={this.props.promotions.isLoading}
+                        />
+                </Animated.View>
+                <Animated.View style={{width: '100%', transform: [{ translateX: xpos3}]}}>
+                    <RenderItem
+                        item = { this.props.leaders.leaders.filter((leader) => leader.featured)[0] }
+                        errMess={this.props.leaders.errMess}
+                        isLoading={this.props.leaders.isLoading}
+                        />
+                </Animated.View>
+            </View>
         );
     }
 }
