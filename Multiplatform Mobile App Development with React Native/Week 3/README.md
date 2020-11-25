d278d3a50d4f23251d01c211dc8ec9126fc9c1ce
alistair0111 <alistair.baretto@gmail,com>
2020-11-25 21:22:00 +0530

    Persist Redux Store

--- Multiplatform Mobile App Development with React Native/Week 3/README.md
+++ Multiplatform Mobile App Development with React Native/Week 3/README.md
@@ -4,10 +4,17 @@
 2. https://github.com/dancormier/react-native-swipeout
 
 # React Native Resources { Animations }
+
 1. https://reactnative.dev/docs/animations
 2. https://reactnative.dev/docs/animations
 3. https://github.com/oblador/react-native-animatable
 
 # React Native Resources { Gestures }
+
 1. https://reactnative.dev/docs/panresponder
 2. https://github.com/oblador/react-native-animatable
+
+# Redux Resources 
+
+1. https://github.com/rt2zz/redux-persist
+2. https://blog.reactnativecoach.com/the-definitive-guide-to-redux-persist-84738167975?gi=ba4d905d2a13
--- Multiplatform Mobile App Development with React Native/Week 3/ReactNativeApp/confusion/App.js
+++ Multiplatform Mobile App Development with React Native/Week 3/ReactNativeApp/confusion/App.js
@@ -1,15 +1,22 @@
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 import React from 'react';
-import Main from './components/MainComponent'
+import Main from './components/MainComponent';
+import { PersistGate } from 'redux-persist/es/integration/react'
+import { Loading } from './components/LoadingComponent';
 
-const store = ConfigureStore();
 
+const { store, persistor } = ConfigureStore();
 
 export default function App() {
   return (
     <Provider store = {store}>
-      <Main />
+      <PersistGate
+        loading={<Loading/>}
+        persistor={persistor}
+        >
+        <Main />
+      </PersistGate>
     </Provider>
   );
 }
