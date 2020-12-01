46e2be21147652801c8f837413e8302904e9ff20
alistair0111 <alistair.baretto@gmail,com>
2020-12-02 01:13:10 +0530

    Sending Email

--- Multiplatform Mobile App Development with React Native/Week 4/ReactNativeApp/confusion/components/ContactComponent.js
+++ Multiplatform Mobile App Development with React Native/Week 4/ReactNativeApp/confusion/components/ContactComponent.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import { Card } from 'react-native-elements';
-import { Text, View } from 'react-native';
+import { Card, Icon } from 'react-native-elements';
+import { Button, Text, View } from 'react-native';
 import * as Animatable from 'react-native-animatable';
-
+import * as MailComposer from 'expo-mail-composer';
 
 class Contact extends Component {
+
     
     static navigationOptions = {
         title: 'Contact Us'
     };
+
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern'
+        });
+    }
 
     render() {
         return(
@@ -21,6 +30,12 @@ class Contact extends Component {
                         <Text style={{margin:10}}>Tel: +852 1234 5678</Text>
                         <Text style={{margin:10}}>Fax: +852 8765 4321</Text>
                         <Text style={{margin:10}}>Email:confusion@food.net</Text>
+                        <Button 
+                            title='Send Email'
+                            buttonStyle = {{backgroundColor: '#512DAB'}}
+                            icon={<Icon name="envelope-o" type='font-awesome' color="white"/>}
+                            onPress= {this.sendMail}
+                            />
                     </Card>
                 </Animatable.View>
             </View>
