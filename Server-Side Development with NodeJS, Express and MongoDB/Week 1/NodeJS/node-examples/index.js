bca95ebb6b8ef87b630240924a8fe1a29bf104cb
alistair0111 <alistair.baretto@gmail,com>
2020-12-04 21:00:28 +0530

    node callbacks and error handling

--- Server-Side Development with NodeJS, Express and MongoDB/Week 1/NodeJS/node-examples/index.js
+++ Server-Side Development with NodeJS, Express and MongoDB/Week 1/NodeJS/node-examples/index.js
@@ -1,18 +1,18 @@
-var rect = {
-    perimiter: (x,y) => (2*(x+y)),
-    area: (x,y) => (x*y)
-};
+var rect = require('./rectangle');
 
 function solveRect(l,b) {
     console.log("Solving for rectangle with l=" +l+" and b="+b);
+    rect(l,b,(err, rectangle) => {
+        if (err) {
+            console.log("Error: ", err.message);
+        }
+        else {
+            console.log("The area of the rectangle of dimensions l="+l+" and b="+b+" is: "+rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l="+l+" and b="+b+" is: "+rectangle.perimeter());
 
-    if (l<=0 || b<=0) {
-        console.log("Dimensions should be greater than zero")
-    }
-    else {
-        console.log("The area of the rectangle is: "+rect.area(l,b));
-        console.log("the perimeter of rectangle is: "+rect.perimiter(l,b))
-    }
+        }
+    });
+    console.log("this statement is after the call to rect");
 } 
 
 solveRect(2,4);
