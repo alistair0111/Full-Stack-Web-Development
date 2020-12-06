715ed84447168f354fdb935e86ef554724159996
alistair0111 <alistair.baretto@gmail,com>
2020-12-06 19:39:40 +0530

    Assignment 1

--- Server-Side Development with NodeJS, Express and MongoDB/Week 1/NodeJS/node-express/index.js
+++ Server-Side Development with NodeJS, Express and MongoDB/Week 1/NodeJS/node-express/index.js
@@ -3,35 +3,22 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const dishRouter = require('./routes/dishRouter')
-const hostname = 'localhost'
-const port = 3000
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+
+
+const hostname = 'localhost';
+const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
-
-
-// app.get('/dishes/:dishId',(req, res, next) => {
-//     res.end("Will send details of the dishe: "+req.params.dishId+" to you.");
-// });
-
-// app.post('/dishes/:dishId',(req, res, next) => {
-//     res.statusCode = 403
-//     res.end('PUT operation not supported on /dishes/'+req.params.dishId);
-// });
-
-// app.put('/dishes/:dishId',(req, res, next) => {
-//     res.write('Updating the dish: '+req.params.dishId);
-//     res.end("Will update the dish: "+req.body.name+" with details "+ req.body.description);
-// });
-
-// app.delete('/dishes/:dishId',(req, res, next) => {
-//     res.end('Deleting the dish: '+req.params.dishId)
-// });
 
 app.use(express.static(__dirname +'/public'))
 
