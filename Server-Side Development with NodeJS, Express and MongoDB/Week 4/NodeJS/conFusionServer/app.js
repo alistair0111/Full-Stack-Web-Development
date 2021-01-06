6b5197cc31726665abe72cdee50fb264a0d2e9a1
alistair0111 <alistair.baretto@gmail,com>
2021-01-06 10:23:30 +0530

    Integrating React Client and Server

--- Server-Side Development with NodeJS, Express and MongoDB/Week 4/NodeJS/conFusionServer/app.js
+++ Server-Side Development with NodeJS, Express and MongoDB/Week 4/NodeJS/conFusionServer/app.js
@@ -17,6 +17,7 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 const uploadRouter = require('./routes/uploadRouter');
 const favoriteRouter = require('./routes/favoriteRouter');
+var commentRouter = require('./routes/commentRouter');;
 
 
 const mongoose = require('mongoose');
@@ -67,6 +68,7 @@ app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
 app.use('/favorites',favoriteRouter);
+app.use('/comments',commentRouter);
 
 
 // catch 404 and forward to error handler
--- Server-Side Development with NodeJS, Express and MongoDB/Week 4/NodeJS/conFusionServer/models/dishes.js
+++ Server-Side Development with NodeJS, Express and MongoDB/Week 4/NodeJS/conFusionServer/models/dishes.js
@@ -4,25 +4,6 @@ const Schema = mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
 var Currency = mongoose.Types.Currency;
 
-var commentSchema = new Schema({
-    rating:  {
-        type: Number,
-        min: 1,
-        max: 5,
-        required: true
-    },
-    comment:  {
-        type: String,
-        required: true
-    },
-    author:  {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }
-}, {
-    timestamps: true
-});
-
 
 var dishSchema = new Schema({
     name: {
@@ -54,8 +35,7 @@ var dishSchema = new Schema({
     featured: {
         type: Boolean,
         default:false      
-    },
-    comments:[commentSchema]
+    }
 }, {
     timestamps: true
 });
